820ea4502e46fad3fe51e80b5218ef3359eea5f1
jose angel linarez <ydauracontreras@gmail.com>
2020-11-12 23:33:55 -0300

    first commit

---
+++ post-crud/src/test/java/ar/alkemy/com/postcrud/PostCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.alkemy.com.postcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
