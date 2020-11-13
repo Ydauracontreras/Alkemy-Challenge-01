8312cbb7c644b28e988212205d9345cc9feeb93d
jose angel linarez <ydauracontreras@gmail.com>
2020-11-13 00:41:16 -0300

    web methods for list all post, get a post by Id, delet and update Generic Response for events

--- post-crud/src/main/java/ar/alkemy/com/postcrud/constrollers/PostRestController.java
+++ post-crud/src/main/java/ar/alkemy/com/postcrud/constrollers/PostRestController.java
@@ -1,5 +1,8 @@
 package ar.alkemy.com.postcrud.constrollers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.*;
@@ -36,6 +39,77 @@ public class PostRestController {
         r.message = "Post creado con exito";
         r.postId = post.getId();
         return ResponseEntity.ok(r);
+    }
+
+    @GetMapping("/posts")
+    public ResponseEntity<?> listAllPost() {
+        GenericResponse r = new GenericResponse();
+        List<PostResponse> posts = postServiceImp.findAll().stream().map(p -> {
+            PostResponse post = new PostResponse();
+            post.id = p.getId();
+            post.titulo = p.getTitulo();
+            post.categoria = p.getCategoria();
+            post.imagen = p.getImagen();
+            post.fechaDeCreacion = p.getFechaDeCreacion();
+            return post;
+        }).collect(Collectors.toList());
+        if (posts.size() == 0) {
+            r.isOk = false;
+            r.message = " No existen post Registrados";
+        }
+        return ResponseEntity.ok(posts);
+    }
+
+    @GetMapping("/posts/{id}")
+    public ResponseEntity<?> findById(@PathVariable Integer id) {
+        GenericResponse r = new GenericResponse();
+        Post post = postServiceImp.findById(id);
+        if (post != null) {
+            return ResponseEntity.ok(post);
+        }
+        r.isOk = false;
+        r.message = "El post indicado no existe";
+        return ResponseEntity.badRequest().body(r);
+
+    }
+
+    @DeleteMapping("posts/{id}")
+    public ResponseEntity<?> deletedPost(@PathVariable Integer id) {
+        GenericResponse r = new GenericResponse();
+        Post post = postServiceImp.findById(id);
+        if (post != null) {
+            postServiceImp.delete(id);
+            r.isOk = true;
+            r.id = post.getId();
+            r.message = "Eliminaste con existo el Post";
+            return ResponseEntity.ok(r);
+        }
+        r.isOk = false;
+        r.message = "El post indicado no existe";
+        return ResponseEntity.badRequest().body(r);
+    }
+
+    @PatchMapping("posts/{id}")
+    public ResponseEntity<?> updatePost(@PathVariable Integer id, @RequestBody Post pN) {
+        GenericResponse r = new GenericResponse();
+        Post postO = postServiceImp.findById(id);
+        if (postO == null) {
+            r.isOk = false;
+            r.message = "El post indicado no existe";
+            return ResponseEntity.badRequest().body(r);
+        }
+        boolean resultado = postServiceImp.updatePost(id, pN);
+        if (resultado) {
+            r.isOk = true;
+            r.id = postO.getId();
+            r.message = "Editaste con existo el Post";
+            return ResponseEntity.ok(r);
+        }
+
+        r.isOk = false;
+        r.message = "No se puedo actualizar el post indicado";
+        return ResponseEntity.badRequest().body(r);
+
     }
 
 }
---
+++ post-crud/src/main/java/ar/alkemy/com/postcrud/models/response/PostResponse.java
@@ -0,0 +1,11 @@
+package ar.alkemy.com.postcrud.models.response;
+
+import java.util.Date;
+
+public class PostResponse {
+    public Integer id;
+    public String titulo;
+    public String categoria;
+    public String imagen;
+    public Date fechaDeCreacion;
+}
